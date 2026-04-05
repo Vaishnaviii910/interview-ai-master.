29597757be1f3b6921b39a01cd817182d3a5a8bf
vaishnaviii910 <vaishnavisingh4956@gmail.com>
2026-04-06 03:16:11 +0530

    Inital Commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+ const cookieparser = require("cookie-parser")
+const cors = require("cors")
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cookieparser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+/* require all the routes here */
+const authRouter = require('./route/auth.routes')
+
+
+
+// using all the routes here
+app.use("/api/auth",authRouter)
+
+module.exports = app
---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique :[true, "Username already taken"],
+        required:true,
+    },
+    email:{
+        type: String,
+        unique:[true,"Account with this email already existed"],
+        required:true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+module.exports = userModel
