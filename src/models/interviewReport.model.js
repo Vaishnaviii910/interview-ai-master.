511ae27ff8a65f8d88b69ea1fab4833825953bb7
vaishnaviii910 <vaishnavisingh4956@gmail.com>
2026-04-10 20:20:13 +0530

    Added InterviewReport schema and Gemini AI SDK setup

---
+++ src/models/interviewReport.model.js
@@ -0,0 +1,127 @@
+const mongoose = require('mongoose');
+
+/**
+ * - job description schema:string
+ * - resume text:string
+ * - self description:string
+ * 
+ * -matchscore : Number
+ * 
+ * - Technical questions : 
+ *                  [{
+ *                   question : ""
+ *                   intention : ""
+ *                  answer: ""
+ *                  }]
+ * - Behavioral questions : [
+                    {
+ *                   question : ""
+ *                   intention : ""
+ *                  answer: ""
+ *                  }
+    ]
+ * - skill gaps : [{
+                    skill: ""
+                    severity : {
+                    type:string
+                    enum:["low","medium","high"]
+                    }
+                    }]
+ * - preparation plan : [{
+                    day:Number
+                    focus: String
+                    tasks : [string]
+                    }]
+ */
+
+const technicalQuestionSchema = new mongoose.Schema({
+    question:{
+        type: String,
+        required :[true, "Technical question is required"]
+    },
+    intention:{
+        type: String,
+        required : [true,"Intention is required"]
+    },
+    answer:{
+        type:String,
+        required: [true,"Answer is required"]
+    }
+},{
+    _id:false
+}) 
+
+const behavioralQuestionSchema = new mongoose.  Schema({
+    question:{
+        type: String,
+        required :[true, "Technical question is required"]
+    },
+    intention:{
+        type: String,
+        required : [true,"Intention is required"]
+    },
+    answer:{
+        type:String,
+        required: [true,"Answer is required"]
+    }
+},{
+    _id:false
+})
+
+const skillGapSchema = new mongoose.Schema({
+    skill :{
+        type:String,
+        required: [true,"Skill is required"]
+    },
+    severity:{
+        type:String,
+        enum:["low","medium",high],
+        required: [true,"Severity is required"]
+    }
+},{
+    _id:false
+})
+
+const preparationPlanSchema = new mongoose.Schema({
+    day:{
+        type:Number,
+        required: [true,"Day is required"]
+    },
+    focus:{
+        type:String,
+        required : [true,"Focus is required"]
+    },
+    tasks:[{
+        type:String,
+        required: [true,"Task is required"]
+    }]
+
+})
+
+const interviewReportSchema = new mongoose.Schema({
+    jobDescription: {
+        type : String,
+        required: [true,"Job description is required"]
+    },
+    resume:{
+        type : String,
+
+    },
+    selfDescription:{
+        type:String,
+    },
+    matchScore:{
+        type : Number,
+        min:0,
+        max:100,
+    },
+    TechnicalQuestion :[technicalQuestionSchema],
+    behavioralQuestion : [behavioralQuestionSchema],
+    skillGaps : [skillGapSchema],
+    preparationPlan: [preparationPlanSchema]
+},{
+    timestamps:true
+})
+
+const interviewReportModel = mongoose.model("InterviewReport",interviewReportSchema);
+module.exports = interviewReportModel;
