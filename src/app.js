60b4469ec0633291352f824d9569b6e1ee8c07a4
vaishnaviii910 <vaishnavisingh4956@gmail.com>
2026-04-27 17:33:37 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -8,9 +8,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieparser())
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }))
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    // Change: Use an environment variable for the frontend URL
+    origin: process.env.FRONTEND_URL, 
+    credentials: true
 }))
 /* require all the routes here */
 const authRouter = require('./route/auth.routes')
